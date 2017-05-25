c091bb57014f18e1a30479b2e293f3e992eb1532
RBSLsiddharth <siddharth@readybytes.in>
2017-05-25 16:15:53 +0530

    first commit on blog

---
+++ resources/views/successfullyupload.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Todo List</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+@include('Mainview')
+<div class="container">
+    <h2 align="center"> -------------------- UPLOADED SUCCESSFULLY!!!-------------------- </h2>
+    <br/><br/>
+
+
+</div>
+
+</body>
+</html>
